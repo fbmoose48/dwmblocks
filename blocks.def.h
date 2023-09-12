7767b46e48f2e8bb88cddcd686f65a542b256cd8
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2023-09-12 19:28:33 +0000

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "weather.sh 03053",	1800,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" ", "checkupdates | wc -l",	14400,		0},
+
+	{"", "curl ipinfo.io/ip 1",	120,		0},
+
+	{"", "date '+%a %b %d %R'",					30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
